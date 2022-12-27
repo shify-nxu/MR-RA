f7b520acd598af229c1e4f81245e1739513cca6e
shify-nxu <shify_nux@163.com>
2022-12-27 09:00:05 +0800

    Annotation

---
+++ Annotation_plot/S03_runpygenometracks.sh
@@ -0,0 +1,3 @@
+conda active pyGenomeTracks
+pyGenomeTracks  --tracks ./01_genomic_tracks/tracks-use.ini  --region chr11:95558258-95598000 --outFileName rs4409785.pdf --width 20 --height 25
+pyGenomeTracks  --tracks ./01_genomic_tracks/tracks-use-hic.ini  --region chr10:122360394-122400394 --outFileName rs3850765.pdf --width 20 --height 25
---
+++ Annotation_plot/S04_runpygenometracks.sh
@@ -0,0 +1,4 @@
+conda active pyGenomeTracks
+pyGenomeTracks  --tracks tracks_clean.ini  --region chr11:95568258-95588258 --outFileName test.pdf --width 20 --height 20
+pyGenomeTracks  --tracks ./01_genomic_tracks/tracks-use.ini  --region chr11:95558258-95598000 --outFileName rs4409785.pdf --width 20 --height 25
+pyGenomeTracks  --tracks ./01_genomic_tracks/tracks-use-hic.ini  --region chr10:122360394-122400394 --outFileName rs3850765.pdf --width 20 --height 25
